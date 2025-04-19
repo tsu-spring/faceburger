3542289c2996adeea12448865522cd6bec1cc838
Vakho <vakho10@gmail.com>
2025-04-19 13:14:32 +0400

    First code commit

---
+++ src/main/java/ge/tsu/faceburger/FaceburgerApplication.java
@@ -0,0 +1,13 @@
+package ge.tsu.faceburger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceburgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceburgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ge/tsu/faceburger/FaceburgerApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.tsu.faceburger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceburgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
